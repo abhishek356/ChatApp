4e8a9de65204a30b7684c146a487a84570352628
abhishek <abhishek867492@gmail.com>
2025-08-18 00:33:56 +0530

    feature/improved_landing_page: work in progress

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -1,22 +1,38 @@
 import Login from "./Login"
-
+import { RiChatSmile3Fill, RiMenuLine } from "@remixicon/react"
 
 export default function () {
     
     return (<>
-    <div style = {{display:'flex', justifyContent:'center', alignItems:'center', backgroundColor:'red',
-    position:'absolute',
-    top:'50%',
-    left:'50%',
-    transform:'translate(-50%, -50%)',
-    width:'100%',
-    height:'100%'
-    }}>
-        <div style = {{width:'20%', height:'20%', border: '2px solid black', display:'flex', flexDirection:'column',justifyContent:'space-around',gap:'20px',padding:'20px 0'}}>
+    <div className = "main w-full h-screen bg-white font-['gilroy']" >
+        {/* <div style = {{width:'20%', height:'20%', border: '2px solid black', display:'flex', flexDirection:'column',justifyContent:'space-around',gap:'20px',padding:'20px 0'}}>
 
 <button>Signup</button>
 <Login/>
             
+        </div> */}
+        <div className = "container mx-auto">
+            <div className = "navbar w-4/5 mx-auto py-5 px-8 bg-white flex items-center justify-between">
+            <h1 className = "text-2xl font-semibold">Chat App</h1>
+            <div className="links hidden md:flex gap-4">
+                <a className = "text-sm"href="#">Login</a>
+                <a className = "text-sm"href="#">Signup</a>
+                <a className = "text-sm"href="#">Contact Us</a>
+                <a className = "text-sm"href="#">help</a>
+            </div>
+            <RiMenuLine size = {36}
+            color = 'black'
+            className = "lg:hidden"/>
+            </div>
+            <div className = "w-full flex flex-col items-center">
+                <div className = "image w-60 h-60 rounded-full mt-10 flex items-center justify-center">
+                    <RiChatSmile3Fill 
+                    size = {900}
+                    color = "black"
+                    className = "smileLogo"
+                    />
+                </div>
+            </div>
         </div>
     </div>
     </>)
